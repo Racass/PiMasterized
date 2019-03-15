ea9f4eb9772edb96223d4a20dbbdae1eee0dd531
racass <racass>
2019-03-15 15:21:06 -0300

    dash

--- PiMasterizedPlugin.php
+++ PiMasterizedPlugin.php
@@ -1,6 +1,6 @@
 <?php
     require 'dashBoard.php';
-    class PiVPNPlugin extends Plugin
+    class PiMasterizedPlugin extends Plugin
     {
         public function __construct(){
             //this is required by the plugin system to get working properly. This adds all the below events to global like {Class}::{onEventName};
