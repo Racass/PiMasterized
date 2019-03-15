30242590afb3bcace2f4c9b15c67ee282b07c86d
racass <racass>
2019-03-15 15:19:11 -0300

    dash

--- PiMasterizedPlugin.php
+++ PiMasterizedPlugin.php
@@ -5,6 +5,7 @@
         public function __construct(){
             //this is required by the plugin system to get working properly. This adds all the below events to global like {Class}::{onEventName};
             parent::__construct(); //Required
+            echo '<!-- teste -->';
             return true;
         }
 
